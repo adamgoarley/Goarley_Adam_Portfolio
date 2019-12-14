9de1e93953209a82f2e916c2e6449407816de613
Adam Goarley <agoarley@icloud.com>
2019-12-14 13:33:22 -0500

    final push

--- config.js
+++ config.js
@@ -6,12 +6,4 @@ const config = {
     dbase: "db_final"
 }
 
-// const config = {
-//     host: "us-cdbr-iron-east-05.cleardb.net", 
-//     port: 8889, 
-//     uname: "b1723d58c20307", 
-//     pword: "0bc4ba57", 
-//     dbase: "heroku_04e54b97da6318d"
-// }
-
 module.exports = config;
