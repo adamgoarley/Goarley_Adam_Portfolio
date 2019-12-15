795c1ce956278d6c0ebc3bc6c63df4c204d38ff4
Adam Goarley <agoarley@icloud.com>
2019-12-14 21:21:18 -0500

    adding files for heroku

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 const config = {
-    host: "localhost", 
+    host: "us-cdbr-iron-east-05.cleardb.net", 
     port: 8889, 
-    uname: "root", 
-    pword: "root", 
-    dbase: "db_final"
+    uname: "ba64170cf2ec22", 
+    pword: "d249c492", 
+    dbase: "heroku_ea5c37ca652c0e9"
 }
+
+// const config = {
+//     host: "localhost", 
+//     port: 8889, 
+//     uname: "root", 
+//     pword: "root", 
+//     dbase: "db_final"
+// }
 
 module.exports = config;
